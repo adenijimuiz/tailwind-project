1a2676bbb702ac1c3cbb69e8ea2afe69530a173b
adenijimuiz <muizodunayo111@gmail.com>
2025-01-15 12:06:11 +0100

    update

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', //user will be able to control th
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
