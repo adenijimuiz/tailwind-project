7d43e584b8d3702879015b94bd7f70f0d70d914d
adenijimuiz <muizodunayo111@gmail.com>
2025-01-22 12:56:04 +0100

    footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class', //user will be able to control th
+  darkMode: 'class', //user will be able to control the mode between light and dark
   content: ["*"],
   theme: {
     extend: {},
